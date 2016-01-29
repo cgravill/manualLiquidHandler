2b6e2ef86ef592f6c57e877daacb9efdb991d3b0
ddn0 <d.nguyen@synthace.com>
2016-01-29 17:30:30 +0000

    add test

---
+++ test_scripts/01_run_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import subprocess
+import os
+import sys
+
+basedir = os.path.dirname(sys.argv[0])
+os.chdir(basedir)
+subprocess.check_call(['antharun', '-workflow', '01_workflow.json', '-parameters', '01_parameters.json', '-driver', 'localhost:50051'])
