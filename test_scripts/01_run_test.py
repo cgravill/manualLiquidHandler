92d3ab4d1d7dca352696856f9f5bb2deb616a5e3
ddn0 <d.nguyen@synthace.com>
2016-03-02 10:29:07 +0000

    hack test protocol volumes

--- test_scripts/01_run_test.py
+++ test_scripts/01_run_test.py
@@ -6,5 +6,4 @@ import sys
 basedir = os.path.dirname(sys.argv[0])
 if basedir:
     os.chdir(basedir)
-# DDN: Disable for now because planner requires head adaptors not supported by this driver
-#subprocess.check_call(['antharun', '--workflow', '01_workflow.json', '--parameters', '01_parameters.json', '--driver', 'localhost:50051'])
+subprocess.check_call(['antharun', '--workflow', '01_workflow.json', '--parameters', '01_big_parameters.json', '--driver', 'localhost:50051'])
