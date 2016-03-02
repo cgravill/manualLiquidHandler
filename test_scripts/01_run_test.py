d827b7ec4c05445ce003b8f76c2b65f7d4a84193
ddn0 <d.nguyen@synthace.com>
2016-03-02 10:07:38 +0000

    fix test

--- test_scripts/01_run_test.py
+++ test_scripts/01_run_test.py
@@ -4,5 +4,6 @@ import os
 import sys
 
 basedir = os.path.dirname(sys.argv[0])
-os.chdir(basedir)
-subprocess.check_call(['antharun', '-workflow', '01_workflow.json', '-parameters', '01_parameters.json', '-driver', 'localhost:50051'])
+if basedir:
+    os.chdir(basedir)
+subprocess.check_call(['antharun', '--workflow', '01_workflow.json', '--parameters', '01_parameters.json', '--driver', 'localhost:50051'])
